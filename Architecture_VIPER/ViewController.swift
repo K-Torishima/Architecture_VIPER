4a62cbb28496d9fae415673ce81ed46423b53927
koji-torishima <torikou3685@gmail.com>
2021-08-16 19:36:59 +0900

    Initial Commit

---
+++ Architecture_VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Architecture_VIPER
+//
+//  Created by 鳥嶋 晃次 on 2021/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
