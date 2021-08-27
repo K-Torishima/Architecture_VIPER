b4a42b085ffaee6703a3c6bddfa56bb6177b85bf
koji-torishima <torikou3685@gmail.com>
2021-08-27 17:44:12 +0900

    ViewControllerを仮実装する

--- Architecture_VIPER/Module/SearchRepository/View/SearchRepositoryViewController.swift
+++ Architecture_VIPER/Module/SearchRepository/View/SearchRepositoryViewController.swift
@@ -5,4 +5,25 @@
 //  Created by 鳥嶋 晃次 on 2021/08/16.
 //
 
-import Foundation
+import UIKit
+
+// ViewControllerはUIのことしかしない
+// 基本的には、Presenterからの指示を受ける
+
+class SearchRepositoryViewController: UIViewController {
+    
+    var presenter: SearchRepositoryPresenterInput!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+extension SearchRepositoryViewController : SearchRepositoryPresenterOutput {
+    
+}
