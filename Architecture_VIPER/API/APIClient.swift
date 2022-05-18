bb79aed550cee72057eaaa3316d7e4965a7a132a
torishima <torikou3685@gmail.com>
2022-05-18 13:19:37 +0900

    APIクライアントにasync methodを生やす

--- Architecture_VIPER/API/APIClient.swift
+++ Architecture_VIPER/API/APIClient.swift
@@ -15,6 +15,7 @@ class APIClient {
         self.session = session
     }
     
+    /// collback method
     @discardableResult
     func request<T: RequestProtocol>(_ request: T,
                                      completion: @escaping (Result<T.ResponseType, Error>) -> ()) -> URLSessionTask? {
@@ -74,5 +75,52 @@ class APIClient {
         }
         task.resume()
         return task
+    }
+    
+    /// async method
+    func request<T: RequestProtocol>(_ request: T) async throws -> T.ResponseType {
+        return try await withCheckedThrowingContinuation { continuation in
+            let url = request.baseURL.appendingPathComponent(request.path)
+            guard var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+                continuation.resume(throwing: APIError.failedToCreateComponents(url))
+                return
+            }
+            components.queryItems = request.queryParameters?.compactMap(URLQueryItem.init)
+            guard var urlRequest = components.url.map({ URLRequest(url: $0) }) else {
+                continuation.resume(throwing: APIError.failedToCreateURL(components))
+                return
+            }
+            urlRequest.httpMethod = request.method.rawValue
+            if let body = request.body {
+                urlRequest.httpBody = body
+            }
+            urlRequest.allHTTPHeaderFields = request.header
+            let task = session.dataTask(with: urlRequest) { data, response, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+                guard let response = response as? HTTPURLResponse else {
+                    continuation.resume(throwing: APIError.noResponse)
+                    return
+                }
+                guard let data = data else {
+                    continuation.resume(throwing: APIError.noData(response))
+                    return
+                }
+                guard  200..<300 ~= response.statusCode else {
+                    let message = try? JSONDecoder().decode(APIError.Message.self, from: data)
+                    continuation.resume(throwing: APIError.unacceptableStatusCode(response.statusCode, message))
+                    return
+                }
+                do {
+                    let object = try JSONDecoder().decode(T.ResponseType.self, from: data)
+                    continuation.resume(returning: object)
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+            task.resume()
+        }
     }
 }
